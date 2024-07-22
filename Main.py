ddbe280e9f27934ab5d737f977fbe9ea6aec0d95
Dylan <dylan.r8uax@slmail.me>
2024-07-22 23:48:43 +0000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='!')  # Set command prefix to be '!'
+bot = commands.Bot(command_prefix='/')
 
 @bot.event
 async def on_ready():
@@ -9,11 +9,11 @@ async def on_ready():
 
 @bot.event
 async def on_message(ctx):
-    if ctx.author == bot.user:  # Ignore messages from the bot itself
+    if ctx.author == bot.user:
         return
 
-    if ctx.content.startswith('!ping'):  # Check if message starts with
+    if ctx.content.startswith('!ping'):
 '!ping'
-        await ctx.send('Hello')  # Reply with 'Hello'
+        await ctx.send('Hello')
 
 bot.run('YOUR_BOT_TOKEN')
